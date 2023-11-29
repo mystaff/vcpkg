2dda8105b3807de159e6f70e9b37a5321d79ce78
Rashad KM <kmrashad@users.noreply.github.com>
2023-11-29 06:37:53 +0530

    fix msys sha hash

--- scripts/cmake/vcpkg_find_acquire_program.cmake
+++ scripts/cmake/vcpkg_find_acquire_program.cmake
@@ -534,7 +534,7 @@ function(vcpkg_find_acquire_program program)
                         "https://repo.msys2.org/mingw/i686/mingw-w64-i686-pkg-config-${VERSION}-any.pkg.tar.zst"
                         0c086bf306b6a18988cc982b3c3828c4d922a1b60fd24e17c3bead4e296ee6de48ce148bc6f9214af98be6a86cb39c37003d2dcb6561800fdf7d0d1028cf73a4
                         "https://repo.msys2.org/mingw/i686/mingw-w64-i686-libwinpthread-${program_version}-any.pkg.tar.zst"
-                        c5a008db60305341254b617dfd60528ebc5d6b02a55bf63e5ca5ac76a73448d5ceb4baf2223869ee86f184242c5287da98545ab2bfc15b4d2f54ce1e23652997
+                        a4a1b268f4e054fe514186eb88e9ccc2963a897082d315289c8970ccee69ae38a5028cf797b99fde1022dc77e0b94f9d4b1da37228ec88b0e3
                 )
             endif()
             set("${program}" "${PKGCONFIG_ROOT}/mingw32/bin/pkg-config.exe" CACHE INTERNAL "")
